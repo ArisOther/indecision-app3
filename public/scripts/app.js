f874a55a7545d172aa9d90c34f1fb1bc1ee023f4
aris winandi <winandiaris@gmail.com>
2019-07-02 21:22:58 +0700

    win src

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,149 +1,118 @@
 "use strict";
 
-function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+console.log('hello world'); //menggunakan function
+//--fungsi alamat
+//menggunakan if statement (if...else...)
+//--fungsi alamat: getAlamat(alamatku), kondisi 'alamatku' harus terisi. dalam bentuk apapun. misalnya,dibawah ini mengambil 'dataKaryawan.Alamat' sebagai isi dari kondisi
+//--IF 'alamatku' terpenuhi/terisi maka return/ditampilkan 'alamatku' itu sendiri
+//--ELSE 'alamatku' tidak terpenuhi/kosong, maka return/ditampilkan string 'tidak diketahui'
+//menggunakan conditional (ternary) operator
+//--variablename = (condition) ? value1:value2 
+//--templateTwo = dataKaryawan.nama ? dataKaryawan.nama : 'tidak diketahui'
+//--IF 'dataKaryawan.nama' eksis, maka value1= terisi 'dataKaryawan.nama' itu sendiri, dan jika tidak eksis, value2='tidak diketahui'
+// menggunakan logical && operator
+//template1
+//Hanya me render subtitle (dan 'p' tag) jika subtitle eksis - 
+//merender p tag baru - jika options.length > 3 (array) maka muncul "ini dia pilihanmu", jika kurang dari itu muncul "ndak ada pilihan"
+//JSX - Javascript XML versi git
 
-function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+var app = {
+  title: 'Indecision App1',
+  subtitle: 'serahkan nyawamu',
+  options: ['one', 'two'] //Fungsi untuk mengecek input
 
-function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+};
 
-function _get(target, property, receiver) { if (typeof Reflect !== "undefined" && Reflect.get) { _get = Reflect.get; } else { _get = function _get(target, property, receiver) { var base = _superPropBase(target, property); if (!base) return; var desc = Object.getOwnPropertyDescriptor(base, property); if (desc.get) { return desc.get.call(receiver); } return desc.value; }; } return _get(target, property, receiver || target); }
+var onFormSubmit = function onFormSubmit(e) {
+  e.preventDefault();
+  var option = e.target.elements.option.value;
 
-function _superPropBase(object, property) { while (!Object.prototype.hasOwnProperty.call(object, property)) { object = _getPrototypeOf(object); if (object === null) break; } return object; }
-
-function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
-
-function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
-
-var Person =
-/*#__PURE__*/
-function () {
-  //Definisi variabel2 yang akan di olah
-  function Person() {
-    var name = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'kosong';
-    var age = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
-
-    _classCallCheck(this, Person);
-
-    this.name = name;
-    this.age = age;
+  if (option) {
+    app.options.push(option);
+    e.target.elements.option.value = '';
+    render();
   }
+};
 
-  _createClass(Person, [{
-    key: "getGreeting",
-    value: function getGreeting() {
-      return "hai i am ".concat(this.name);
-    } //method yang akan mengolah definisi variabel diatas
+var onRemoveAll = function onRemoveAll() {
+  app.options = [];
+  render();
+};
 
-  }, {
-    key: "getDescription",
-    value: function getDescription() {
-      return "".concat(this.name, " berumur ").concat(this.age, "!");
-    }
-  }]);
-
-  return Person;
-}(); //membuat class Student dengan mengambil referensi Person
-//-constructor() dengan identifier name,age, dan major
-//-super() mengambil ref 'name' dan 'age' dari class person
-//deklarasi variabel asli milik student, yaitu 'Major'
+var onMakeDecision = function onMakeDecision() {
+  var randomNum = Math.floor(Math.random() * app.options.length);
+  var option = app.options[randomNum];
+  alert(option);
+  console.log(randomNum);
+}; //variabel data sumber 'data karyawan'/sumber input data
 
 
-var Student =
-/*#__PURE__*/
-function (_Person) {
-  _inherits(Student, _Person);
+var dataKaryawan = {
+  nama: 'aris winandi',
+  alamat: 'tuban w',
+  jabatan: 'manager',
+  usia: 18,
+  usiaMax: 32
+}; //program counter manual
+//fungsi perhitungan------------------------------------------------------
 
-  function Student(name, age, Major) {
-    var _this;
+var hitung = 0;
 
-    _classCallCheck(this, Student);
+var penambahan = function penambahan() {
+  hitung++;
+  renderTemplate4();
+};
 
-    _this = _possibleConstructorReturn(this, _getPrototypeOf(Student).call(this, name, age));
-    _this.Major = Major;
-    return _this;
-  } //Method untuk memberikan nilai False pada this.Major
-  //-!this.Major = True, !!this.Major=False
+var pengurangan = function pengurangan() {
+  hitung--;
+  renderTemplate4();
+};
+
+var reset = function reset() {
+  hitung = 0;
+  renderTemplate4();
+}; //-------------------------------------------------------------------------
+//fungsi alamat. ---------------------------------------------------------------
 
 
-  _createClass(Student, [{
-    key: "hasMajor",
-    value: function hasMajor() {
-      return !!this.Major;
-    } //-getDescription() untuk menampilkan
-    //-membuat variabel 'let description' dengan nilai menggunakan super() karena
-    //--mengambil nilai dari Person.getDescription
-    //-Mengecek, jika hasMajor()
-    //--Jika hasMajor() terisi = 'aris winandi berumur 26!and has major Matematika'
-    //--Jika hasMajor() tidak terisi = 'aris winandi berumur 26!
-    //--catatan: jika tanpa fungsi hasMajor() tetap akan tampil namun,
-    //-----------jika this.Major tidak terisi, akan tampil 'undefined' :
-    //-----------'aris winandi berumur 26! and has major undefined'
-
-  }, {
-    key: "getDescription",
-    value: function getDescription() {
-      var description = _get(_getPrototypeOf(Student.prototype), "getDescription", this).call(this);
-
-      if (this.hasMajor()) {
-        description = description + "and has major ".concat(this.Major);
-      }
-
-      return description; //return `${description} and has major ${this.Major}`;
-    }
-  }]);
-
-  return Student;
-}(Person);
-
-var homeLoc =
-/*#__PURE__*/
-function (_Person2) {
-  _inherits(homeLoc, _Person2);
-
-  function homeLoc(name, location) {
-    var _this2;
-
-    _classCallCheck(this, homeLoc);
-
-    _this2 = _possibleConstructorReturn(this, _getPrototypeOf(homeLoc).call(this, name));
-    _this2.location = location;
-    return _this2;
+function getAlamat(alamatku) {
+  if (alamatku) {
+    return alamatku;
+  } else {
+    return 'tidak diketahui';
   }
+}
 
-  _createClass(homeLoc, [{
-    key: "hasLoc",
-    value: function hasLoc() {
-      return !!this.location;
-    }
-  }, {
-    key: "Greeting",
-    value: function Greeting() {
-      var GetG = _get(_getPrototypeOf(homeLoc.prototype), "getGreeting", this).call(this);
-
-      if (this.hasLoc()) {
-        GetG = GetG + " i am from ".concat(this.location);
-      }
-
-      return GetG;
-    }
-  }]);
-
-  return homeLoc;
-}(Person); //pemanggilan class dengan parameter lengkap
+function getUsiamax(usiaMaksimal) {
+  if (usiaMaksimal < 18 || usiaMaksimal >= 35) {
+    return 'usia tidak memenuhi syarat';
+  } else {
+    return usiaMaksimal;
+  }
+} //--------------------------------------------------------------------------------
+//data yang di render-------------------------------------------------------------
+//----------------------------------------------------
 
 
-var write = new Student('aris winandi', 27, 'Matematika');
-console.log(write.getDescription()); //pemanggilan class dengan parameter kosong
+var appRoot = document.getElementById('app'); //jangan sampai salah posisi
 
-var other = new Student();
-console.log(other.getDescription());
-var Greeting2 = new homeLoc('Andi Winaris', 'tuban');
-console.log(Greeting2.Greeting());
+var render = function render() {
+  var template1 = React.createElement("div", null, React.createElement("h1", null, app.title), app.subtitle && React.createElement("p", null, app.subtitle), React.createElement("p", null, app.options.length > 0 ? 'inilah pilihanmu' : 'ndak ada pilihan'), React.createElement("p", null, app.options.length), React.createElement("button", {
+    disabled: false,
+    onClick: onMakeDecision
+  }, "Apa yang harus aku lakukan"), React.createElement("button", {
+    onClick: onRemoveAll
+  }, "Remove All"), React.createElement("ol", null, app.options.map(function (option) {
+    return React.createElement("li", {
+      key: option
+    }, option);
+  })), React.createElement("form", {
+    onSubmit: onFormSubmit
+  }, React.createElement("input", {
+    type: "text",
+    name: "option"
+  }), React.createElement("button", null, "add Item")));
+  ReactDOM.render(template1, appRoot); //jangan sampai salah posisi
+};
+
+render();
